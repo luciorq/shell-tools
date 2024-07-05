950cb8432d562c9eeae021c8f358befc49e3281d
luciorq <luciorqueiroz@gmail.com>
2024-07-05 11:18:30 -0400

    fix: add sshd config to remove chatter

--- server_utils/remove_chatter_trojan.sh
+++ server_utils/remove_chatter_trojan.sh
@@ -1,90 +1,91 @@
 #!/usr/bin/env bash
 
-
 # Before starting compile e2fsprogs from GitHub repo
 # + git clone https://github.com/tytso/e2fsprogs
 # + preferentially compile on a different machine
-function __compile_e2fsprogs () {
+function __compile_e2fsprogs() {
   # builtin local mkdir_bin;
-  mkdir -p "${HOME}/workspaces/temp";
-  builtin cd "${HOME}/workspaces/temp" || exit;
-  git clone https://github.com/tytso/e2fsprogs;
-  builtin cd e2fsprogs || exit;
-  mkdir build;
-  builtin cd build || exit;
-  ../configure;
-  make;
-  make check;
-  builtin return 0;
+  mkdir -p "${HOME}/workspaces/temp"
+  builtin cd "${HOME}/workspaces/temp" || exit
+  git clone https://github.com/tytso/e2fsprogs
+  builtin cd e2fsprogs || exit
+  mkdir build
+  builtin cd build || exit
+  ../configure
+  make
+  make check
+  builtin return 0
 }
 
-function __remove_chattr_trojan () {
-  local sudo_bin;
+function __remove_chattr_trojan() {
+  local sudo_bin
   # local ls_bin;
-  local rm_bin;
-  local build_path;
-  local lsattr_bin chattr_bin;
-  local _search_dir_arr;
-  local _still_infected;
-  local _pkg_arr;
-  local _path_to_search;
-  local _i;
-  local _j;
-  local _count;
-  local _file;
-  local _pkg;
+  local rm_bin
+  local build_path
+  local lsattr_bin chattr_bin
+  local _search_dir_arr
+  local _still_infected
+  local _pkg_arr
+  local _path_to_search
+  local _i
+  local _j
+  local _count
+  local _file
+  local _pkg
 
-  sudo_bin='/usr/bin/sudo';
+  sudo_bin='/usr/bin/sudo'
 
-  "${sudo_bin}" builtin echo -ne "Super User\n";
+  "${sudo_bin}" builtin echo -ne "Super User\n"
 
   # ls_bin='/usr/bin/ls';
-  rm_bin='/usr/bin/rm';
-  build_path="${HOME}/workspaces/temp";
-  lsattr_bin="${build_path}/e2fsprogs/build/misc/lsattr";
-  chattr_bin="${build_path}/e2fsprogs/build/misc/chattr";
+  rm_bin='/usr/bin/rm'
+  build_path="${HOME}/workspaces/temp"
+  lsattr_bin="${build_path}/e2fsprogs/build/misc/lsattr"
+  chattr_bin="${build_path}/e2fsprogs/build/misc/chattr"
 
   declare -a _search_dir_arr=(
     /usr/bin/
     /usr/sbin/
     /usr/share/
-  );
+    /etc/ssh/
+    /etc/ssh/sshd_config.d/
+  )
 
-  declare -a _still_infected;
-  builtin echo -ne "Files still infected:\n\n";
+  declare -a _still_infected
+  \builtin echo -ne "Files still infected:\n\n"
 
   # _still_infected=( $("${sudo_bin}" "${ls_bin}" /usr/share/doc/*so* -A1 2> /dev/null | grep '\.so.*') )
-  _still_infected=( );
+  _still_infected=()
 
   for _path_to_search in "${_search_dir_arr[@]}"; do
     _still_infected+=(
-      $("${sudo_bin}" "${lsattr_bin}" -a "${_path_to_search}" 2> /dev/null \
-        | grep 'i--' \
-        | grep -v 'Operation not supported While reading flag' \
-        | grep 'i--' \
-        | cut -d ' ' -f2)
-    );
+      $("${sudo_bin}" "${lsattr_bin}" -a "${_path_to_search}" 2>/dev/null |
+        grep 'i--' |
+        grep -v 'Operation not supported While reading flag' |
+        grep 'i--' |
+        cut -d ' ' -f2)
+    )
   done
 
   for _i in "${_still_infected[@]}"; do
-    builtin echo -ne "${_i}\n";
+    \builtin echo -ne "${_i}\n"
   done
 
   for _file in "${_still_infected[@]}"; do
-    _count=1;
+    _count=1
     while [[ -f ${_file} && ${_count} -lt 100 ]]; do
-      "${sudo_bin}" "${chattr_bin}" -i "${_file}" 2> /dev/null \
-        && "${sudo_bin}" "${rm_bin}" -rf "${_file}" 2> /dev/null;
-      builtin echo -ne "can't remove ${_file}\n";
-      _count=$(( _count + 1 ));
+      "${sudo_bin}" "${chattr_bin}" -i "${_file}" 2>/dev/null &&
+        "${sudo_bin}" "${rm_bin}" -rf "${_file}" 2>/dev/null
+      \builtin echo -ne "can't remove ${_file}\n"
+      _count=$((_count + 1))
     done
-    builtin echo -ne "Removed: '${_file}'\n";
+    \builtin echo -ne "Removed: '${_file}'\n"
   done
 
   # try apt update?
-  "${sudo_bin}" DEBIAN_FRONTEND=noninteractive apt update --yes;
+  "${sudo_bin}" DEBIAN_FRONTEND=noninteractive apt update --yes
   # Fix stuck dpkg
-  "${sudo_bin}" DEBIAN_FRONTEND=noninteractive apt --fix-broken --yes install;
+  "${sudo_bin}" DEBIAN_FRONTEND=noninteractive apt --fix-broken --yes install
 
   declare -a _pkg_arr=(
     cpio
@@ -105,17 +106,17 @@ function __remove_chattr_trojan () {
       --yes \
       --reinstall \
       --purge \
-      "${_pkg}";
+      "${_pkg}"
   done
 
-  builtin echo -ne \
-    "\n |> Install additional programs that appear as removed above.\n";
+  \builtin echo -ne \
+    "\n |> Install additional programs that appear as removed above.\n"
 
   for _j in "${_still_infected[@]}"; do
-    builtin echo -ne "${_j}\n";
+    \builtin echo -ne "${_j}\n"
   done
 
   # "${sudo_bin}" DEBIAN_FRONTEND=noninteractive apt upgrade --yes;
 
-  builtin return 0;
+  \builtin return 0
 }
