e7835e79c89db45aa1b6fc32260134d56882ff3f
luciorq <luciorqueiroz@gmail.com>
2024-12-24 07:35:44 -0500

    docs: add notes on move home zfs

--- server_utils/move_home_disks_zfs.sh
+++ server_utils/move_home_disks_zfs.sh
@@ -1,6 +1,10 @@
 /usr/bin/env bash
 
+# NOTE: Highly distructive script, make sure to read and understant it before running
+
 ZPOOL_NAME="datahomes"
+
+# NOTE: /dev/sdc* was specific for this machine, change it before running
 DISK_WWN_ID="$(\ls -l /dev/disk/by-id/ | \grep 'sdc$' | \awk '{print $9}' | \grep '^wwn')"
 
 \sudo zpool create "${ZPOOL_NAME}" "${DISK_WWN_ID}";
@@ -13,8 +17,7 @@ DISK_WWN_ID="$(\ls -l /dev/disk/by-id/ | \grep 'sdc$' | \awk '{print $9}' | \gre
 \sudo zfs set redundant_metadata=most "${ZPOOL_NAME}";
 \sudo zfs set mountpoint=/tmp_home "${ZPOOL_NAME}";
 
-
-# Copiar arquivos da Home antiga para a home temporarario
+# Copy files from old home to the temporary home dir
 \sudo mount -a;
 \sudo rsync -avhP --delete --info=progress2 /home /tmp_home;
 
